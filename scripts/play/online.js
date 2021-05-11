0d3c9715f8153aa4148f08ed885afb4340f88feb
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-05-12 11:46:43 +1200

    Unhide online section of homepage
    Game ID is set from the options submenu on the homepage.

--- scripts/play/online.js
+++ scripts/play/online.js
@@ -1,18 +1,24 @@
 const apiUrl = '/.netlify/functions/database';
 
-(function getGameId() {
-    return window.gameId = document.cookie.split('; ').find(row => row.startsWith('gameId=')).split('=')[1];
+(function() {
+    document.cookie = 'gameId=' + new URLSearchParams(location.href).get('gamecode');
 })();
 
+function getGameId() {
+    return window.gameId = document.cookie.split('; ').find(row => row.startsWith('gameId=')).split('=')[1];
+}
+
 async function readDB() {
+    const gameId = getGameId();
     const resp = await fetch(`${apiUrl}?type=read&gameId=${gameId}`);
     const json = await resp.json();
     const data = json.output.data;
-    console.debug(`Retrieved FEN data for game ID ${gameId}.`)
+    console.debug(`Retrieved FEN data for game ID ${gameId}.`);
     createBoardFromFen(data.fen);
 }
 
 async function sendDB() {
+    const gameId = getGameId();
     await fetch(`${apiUrl}?type=send&gameId=${gameId}&fen=${encodeURIComponent(createFen())}`);
-    console.debug(`Sent FEN data for game ID ${gameId}.`)
+    console.debug(`Sent FEN data for game ID ${gameId}.`);
 }
