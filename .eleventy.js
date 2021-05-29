db97cdca01faae0db36eaa46b4f391c856505bd1
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-05-29 19:48:53 +1200

    Replace CSS with NovaSheets (#16)
    * Rewrite styles in NovaSheets
    
    * Finish rewrite in NovaSheets
    
    * Compile NovaSheets properly on site build
    
    * Add missing import
    
    * Run afterBuild script synchronously
    
    * Clear all local validation code
    
    * Revert validation deletion
    
    * Fix cell positioning on mobile
    
    * Compile NovaSheets before build
    
    * Unglob compilation
    
    * Move compilation to afterBuild
    
    * Fix afterBuild compilation
    
    * Change afterBuild to beforeBuild
    
    * Update styles
    
    * Use NovaSheets vars instead
    
    * Remove use of @breakpoint
    
    * Remove spaces
    
    * Fix filename in compilation
    
    * Substitute breakpoint var to avoid issues
    
    * Fix spaces from copy-paste issue
    
    * Fix var name
    
    * Update dependencies
    
    * Fix `@each` function
    
    * Bump NovaSheets version
    
    * Workaround for issue
    
    * Simpler workaround
    
    * Fix superfluous commas
    
    * Revert simpler workaround
    
    * Simplify slightly
    
    * Fix breakpoints
    
    * Use breakpoint variable
    
    * Fix cell label positions
    
    * Fix card hover styles not being applied
    
    * Use breakpoint variable
    
    * Align back/play buttons

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,19 @@
+const { execSync } = require('child_process');
+
 module.exports = function (cfg) {
     const passThruPaths = [
         'images/', 'scripts/', 'styles/',
         '_redirects', 'favicon.ico',
     ];
+
     passThruPaths.forEach(file => cfg.addPassthroughCopy(file));
+
+    cfg.on('beforeBuild', () => {
+        ['common', 'home', 'play'].forEach(name => {
+            execSync(`npx novasheets -c styles/${name}.nvss`, (err, stdout, stderr) => console.log(err || stdout));
+        });
+    });
+
     return {
         passthroughFileCopy: true,
         dir: {
