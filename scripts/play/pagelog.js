621ca97c6a90e52db046eb49501a51bf065a6dc3
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-05-31 15:14:04 +1200

    Fix highlighting and points

--- scripts/play/pagelog.js
+++ scripts/play/pagelog.js
@@ -1,12 +1,12 @@
 function log({ startCell, endCell, startClasses, endClasses, count, taken, promoted, castled, check }) {
 
 	window.lastMove = { start: startCell, end: endCell };
+	const [colour, piece] = startClasses;
 
 	if (taken) points[colour[0]] += getPointsEquivalent(endClasses[1]);
 	if (promoted) points[colour[0]] += getPointsEquivalent(getPieceClasses(endCell)[1]);
 	logPoints();
 
-	const [colour, piece] = startClasses;
 	let code = ' ';
 	if (count % 2 === 1 && hasRules) code += '<br class="desktoponly">' + ((count + 1) / 2) + '. ';
 	if (castled) {
