47b44aee1ed7e5b3c590127de65f6cc8845d9246
tenmore <61951543+akatenmore@users.noreply.github.com>
2021-06-03 10:50:11 +1200

    begin implementation of stockfish

--- scripts/play/bot.js
+++ scripts/play/bot.js
@@ -1,72 +1,21 @@
 function botMove(botColour) {
-	const { botIntelligence } = window.gameOptions;
+	var id = 0;
+	var stockfish = require("stockfish");
+	var stockfishes = [];
 
-	let validSquares = [];
-	let validMoves = [];
-	let goodMoves = [];
+	stockfishes[id] = stockfish();
 
-	// Generate valid moves list
-	$$('table .' + botColour).forEach(piece => {
-		const cell = piece.parentNode.id;
-		const moves = findAllMoves(cell)
-		if (moves.length) validSquares.push({ cell, moves });
-	});
-
-	// Generate list of good moves for later use
-	validSquares.forEach(obj => obj.moves.forEach(cell => {
-		const start = obj.cell, end = cell;
-		const march = botIntelligence === 1 && (global.currentTurn === 'w' ? start[1] < end[1] : start[1] > end[1]);
-		const charge = botIntelligence === 2 && pieceInCell(cell);
-		const attack = botIntelligence === 3 && pieceInCell(cell) && getPointsEquivalent(getPieceClasses(start)[1]) <= getPointsEquivalent(getPieceClasses(end)[1]);
-		validMoves.push({ start, end });
-		if (march || charge || attack) goodMoves.push({ start, end });
-	}));
-
-	// If there are no good moves load all valid squares
-	if (goodMoves.length === 0) goodMoves = validMoves;
-
-	// Move piece using various intelligence levels
-	let startCell, endCell;
-	if ([0, 1].includes(botIntelligence) || failedMoveCount >= 3) {
-		// Intelligence level 0: move randomly
-		// Intelligence level 1: prioritise moving forward
-		const selected = goodMoves[random(0, goodMoves.length - 1)];
-		startCell = selected.start;
-		endCell = selected.end;
-	}
-	else if ([2, 3].includes(botIntelligence)) {
-		// Intelligence level 2: prioritise taking pieces
-		// Intelligence level 3: prioritise taking only better pieces
-		goodMoves = goodMoves.map(obj => {
-			const startScore = getPointsEquivalent(getPieceClasses(obj.start)[1]);
-			const endScore = getPointsEquivalent(getPieceClasses(obj.end)[1]) || 0;
-			return { start: obj.start, end: obj.end, score: endScore - startScore };
-		}).sort((a, b) => a.score > b.score ? -1 : +1);
-		if (botIntelligence === 3) {
-			let bestSquares = goodMoves.filter(obj => obj.score >= 0);
-			if (bestSquares.length > 0) goodMoves = bestSquares;
-		}
-		startCell = goodMoves[0].start;
-		endCell = goodMoves[0].end;
+	stockfishes[id].onmessage = function (message) {
+		console.log("received: " + message);
 	}
 
-	// Move the piece
+	stockfishes[id].postMessage('setoption name Contempt value 30');
+	stockfishes[id].postMessage('setoption name Skill Level value 20');
+	stockfishes[id].postMessage('ucinewgame');
+	stockfishes[id].postMessage('isready');
+	stockfishes[id].postMessage('position fen' + global.moveList.slice(-1)[0]);
+	stockfishes[id].postMessage('go');
 	hasClicked(startCell);
 	hasClicked(endCell);
 }
 
-function forceBotMove() {
-	let curFen = createFen();
-	failedMoveCount = 0;
-	do {
-		botMove(global.currentTurn === 'w' ? 'white' : 'black');
-		if (createFen() === curFen) failedMoveCount++;
-	}
-	while (createFen() === curFen && ingame);
-}
-
-setInterval(function () {
-	if (window.gameOptions && window.gameOptions.bot && window.ingame && global.currentTurn === window.gameOptions.botColour[0]) {
-		forceBotMove();
-	}
-}, 650);
