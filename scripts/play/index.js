ea25a89ee206608a09ba12d010f5885df3d3180a
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-06-04 11:08:21 +1200

    Fix variable name

--- scripts/play/index.js
+++ scripts/play/index.js
@@ -40,7 +40,7 @@ function run() {
 	window.gameId = gameOptions.gamecode;
 	window.session = randomID(5);
 	window.username = gameOptions.username || 'Player' + random(0, 999);
-	window.chat = [[+new Date(), session, username, 'joined the game'].join(INFO.SEP)];
+	window.chat = [[+new Date(), session, username, 'joined the game'].join(SEP.INFo)];
 
 	const gameData = $('#game-data dl');
 	const addGameData = (title, content) => gameData.innerHTML += `<dt>${title}</dt><dd>${content}</dd>`;
