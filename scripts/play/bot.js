190631521a92ad840fb6229a64299860e3ccf798
tenmore <61951543+akatenmore@users.noreply.github.com>
2021-06-03 12:05:06 +1200

    implement board movement

--- scripts/play/bot.js
+++ scripts/play/bot.js
@@ -1,21 +1,26 @@
 function botMove() {
+
 	var id = 0;
-
 	var stockfishes = [];
-
 	stockfishes[id] = STOCKFISH();
 
 	stockfishes[id].onmessage = function (message) {
 		console.log("received: " + message);
+		let [, start, end] = message.match(/bestmove (..)(..) /) || [];
+		if (start && end) {
+			hasClicked(start);
+			hasClicked(end);
+		}
 	}
+	//options
+	stockfishes[id].postMessage('setoption name Contempt 20');
+	stockfishes[id].postMessage('setoption name Minimum Thinking Time 1000');
 
-	stockfishes[id].postMessage('setoption name Contempt value 30');
-	stockfishes[id].postMessage('setoption name Skill Level value 20');
 	stockfishes[id].postMessage('ucinewgame');
 	stockfishes[id].postMessage('isready');
 	stockfishes[id].postMessage('position fen ' + global.moveList.slice(-1)[0]);
 	stockfishes[id].postMessage('go');
-	hasClicked(startCell);
-	hasClicked(endCell);
+	//hasClicked(startCell);
+	//hasClicked(endCell);
 }
 
