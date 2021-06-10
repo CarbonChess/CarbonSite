053eb8be95b54d8c92411ab6671f4d66015684e3
tenmore <61951543+akatenmore@users.noreply.github.com>
2021-06-11 10:38:57 +1200

    csv reader

---
+++ scripts/play/puzzles.js
@@ -0,0 +1,18 @@
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = [];
+            for (var j=0; j<headers.length; j++) {
+                tarr.push(headers[j]+":"+data[j]);
+            }
+            lines.push(tarr);
+        }
+    }
+    console.log[lines];
+}
