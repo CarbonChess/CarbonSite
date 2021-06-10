d0728c67a638db0e712c37370f5eeda4aadcaa75
tenmore <61951543+akatenmore@users.noreply.github.com>
2021-06-11 11:07:18 +1200

    added return

--- scripts/play/puzzles.js
+++ scripts/play/puzzles.js
@@ -1,20 +1,20 @@
 function processData(allText) {
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
-    var lines = [];
+    let allTextLines = allText.split(/\r\n|\n/);
+    let headers = allTextLines[0].split(',');
+    let lines = [];
 
-    for (var i = 1; i < allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
+    for (let i = 1; i < allTextLines.length; i++) {
+        let data = allTextLines[i].split(',');
         if (data.length == headers.length) {
 
-            var tarr = [];
+            let tarr = [];
             for (let j = 0; j < headers.length; j++) {
                 tarr.push(headers[j] + ":" + data[j]);
             }
             lines.push(tarr);
         }
     }
-    console.log(lines);
+    return lines;
 }
 
 async function getPuzzles() {
