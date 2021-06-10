6fb5cddc93930505b993e1bdfd62e89bb3068098
tenmore <61951543+akatenmore@users.noreply.github.com>
2021-06-11 10:42:45 +1200

    Update puzzles.js

--- scripts/play/puzzles.js
+++ scripts/play/puzzles.js
@@ -1,18 +1,25 @@
+const fs = require('fs');
+
 function processData(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
     var headers = allTextLines[0].split(',');
     var lines = [];
 
-    for (var i=1; i<allTextLines.length; i++) {
+    for (var i = 1; i < allTextLines.length; i++) {
         var data = allTextLines[i].split(',');
         if (data.length == headers.length) {
 
             var tarr = [];
-            for (var j=0; j<headers.length; j++) {
-                tarr.push(headers[j]+":"+data[j]);
+            for (let j = 0; j < headers.length; j++) {
+                tarr.push(headers[j] + ":" + data[j]);
             }
             lines.push(tarr);
         }
     }
     console.log[lines];
+}
+
+function getPuzzles() {
+    let fileData = fs.readFileSync('/images/puzzles.csv', {encoding: 'utf8' });
+    return processData(fileData);
 }
