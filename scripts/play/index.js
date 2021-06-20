bb2788218eafe7465dce91f149edc94e68a6fa3f
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-06-21 09:46:00 +1200

    Place taken pieces correctly on mobile

--- scripts/play/index.js
+++ scripts/play/index.js
@@ -11,7 +11,7 @@ function run() {
 		botColour: params.get('botColour'),
 		botIntelligence: +params.get('botIntelligence'),
 		multiplayer: booleanParam('multiplayer'),
-		username: params.get('username').trim(),
+		username: params.get('username')?.trim(),
 		rules: !booleanParam('free'),
 		autoFlip: booleanParam('autoflip'),
 		gamecode: params.get('gamecode'),
