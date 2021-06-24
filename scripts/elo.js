9a755f20a2e6a221cb03d6478a748e65f437caab
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-06-24 12:18:54 +1200

    Init user ELO on load

--- scripts/elo.js
+++ scripts/elo.js
@@ -1,11 +1,17 @@
+const DEFAULT_ELO = 1000;
+
 async function saveUserData() {
 	const user_metadata = {
 		data: {
-			elo: window.userELO || 1000,
+			elo: window.userELO || DEFAULT_ELO,
 		}
 	}
 	const user = await netlifyIdentity.gotrue.currentUser().update(user_metadata);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+	window.userELO = netlifyIdentity.gotrue.currentUser()?.user_metadata.elo || DEFAULT_ELO;
+})
 
 netlifyIdentity.on('login', user => {
 	window.userELO = user.user_metadata.elo;
