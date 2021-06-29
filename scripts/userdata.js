f8a105a25181f79f2f0a5178b1d47c85c572b4ac
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-06-29 14:24:38 +1200

    Change default puzzles elo

--- scripts/userdata.js
+++ scripts/userdata.js
@@ -1,27 +1,28 @@
 const DEFAULT_ELO = 1000;
+const DEFAULT_PUZZLE_ELO = 1500;
 
 function initialiseUserData() {
 	const user = netlifyIdentity.gotrue.currentUser();
 	if (!user) return;
 	const { elo, puzzles_elo, full_name } = user.user_metadata;
 	window.userElo = elo || DEFAULT_ELO;
-	window.userPuzzlesElo = puzzles_elo || DEFAULT_ELO;
+	window.userPuzzlesElo = puzzles_elo || DEFAULT_PUZZLE_ELO;
 	window.accountName = full_name;
-	displayAccountInfo();
+	updateDisplayedInfo();
 }
 
 async function saveUserData() {
 	const user_metadata = {
 		data: {
 			elo: window.userElo || DEFAULT_ELO,
-			puzzles_elo: window.userPuzzlesElo || DEFAULT_ELO,
+			puzzles_elo: window.userPuzzlesElo || DEFAULT_PUZZLE_ELO,
 		}
 	}
 	const user = await netlifyIdentity.gotrue.currentUser().update(user_metadata);
-	displayAccountInfo();
+	updateDisplayedInfo();
 }
 
-function displayAccountInfo() {
+function updateDisplayedInfo() {
 	$.id('account-elo').innerText = window.userElo + '/' + window.userPuzzlesElo;
 	$.id('account-username').innerText = window.accountName;
 }
