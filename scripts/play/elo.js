d26d5bae945382a9a91375c379b7df728f16e255
Nixinova <42429413+Nixinova@users.noreply.github.com>
2021-06-29 14:09:22 +1200

    round elo output

--- scripts/play/elo.js
+++ scripts/play/elo.js
@@ -2,5 +2,5 @@ function calculateElo(currentElo, opponentElo, outcome) {
 	const ratingDelta = opponentElo - currentElo;
 	const expected = 1 / (1 + 10 ** (ratingDelta / 400));
 	const multiplier = 30;
-	return currentElo + multiplier * (outcome - expected);
+	return currentElo + Math.round(multiplier * (outcome - expected));
 }
